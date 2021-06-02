5101adc0a995c5e4b13faa1c2962bba26faab181
thangemlun <thien2357@gmail.com>
2021-06-02 09:57:51 +0700

    Fix : Missing Module

--- Mouse.js
+++ Mouse.js
@@ -7,20 +7,4 @@ mouse.prototype.die = function(){
 	this.die = true;
 };
 
-
-function cat(){
-	this.stomach = [] ;
-}
-
-cat.prototype.eat = function(mouse){
-	this.stomach.push(mouse);
-	mouse.die = true ;	
-};
-
-let cat1 = new cat() ;
-let mouse1 = new mouse('jerry',false);
-
-cat1.eat(mouse1);
-
-console.log(cat1.stomach);
-console.log(mouse1.die); 
+module.exports = Mouse;

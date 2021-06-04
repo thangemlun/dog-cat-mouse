a5ecac87d90d59448a687132ce9596bfff0628a7
thangemlun <thien2357@gmail.com>
2021-06-04 19:01:36 +0700

    Delete index.js

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const Dog = require('./Dog.js');
-const Cat = require('./Cat.js');
-const Mouse = require('./Mouse.js');
 
-let catTom = new Cat('Tom');
-
-let mouseJerry = new Mouse('jerry');
-
-let dogPluto = new Dog('Lulu');
-
-catTom.eat(mouseJerry);
-
-dogPluto.eat(catTom);
 

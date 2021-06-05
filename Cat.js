b31f984f9e56ba59501104b1634efb5c0271815e
thangemlun <thien2357@gmail.com>
2021-06-05 13:55:00 +0700

    Add feature/improve cat eat method

--- Cat.js
+++ Cat.js
@@ -1,16 +1,23 @@
-function Cat(name){
-	this.name = name ;
-	this.dead = false ;
-	this.stomach = [] ;
+const Mouse = require('./Mouse.js');
+
+
+function Cat(name) {
+    this.name = name ;
+    this.dead = false ;
+    this.stomach = [] ;
 }
 
-Cat.prototype.die = function(){
-	this.dead = true;
+Cat.prototype.die = function() {
+this.dead = true;
 };
 
-Cat.prototype.eat = function(mouse){
-	this.stomach.push(mouse);
-	mouse.die();
+Cat.prototype.eat = function(animal) {
+if (animal instanceof Mouse) {
+    this.stomach.push(animal);
+    animal.die();
+} else {
+    throw new Error("Can't eat another animal but Mouse !");
+}
 };
 
 module.exports = Cat ;
--- index.js
+++ index.js
@@ -3,4 +3,16 @@ var Cat = require('./Cat.js');
 var Mouse = require('./Mouse.js');
 
 
+let catTom = new Cat('Tom');
+let mouseJerry = new Mouse('Jerry');
+let dog = new Dog();
+
+catTom.eat(mouseJerry);
+try {
+    catTom.eat(dog)
+} catch ( error ) {
+    console.log('Dog too big for cat to eat ! ');
+}
+
+console.log(catTom);
 
